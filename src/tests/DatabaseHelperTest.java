34356b815d475bfeb7b6ecd681980d5c8f3cabe4
Ryan Liu <dio.ryanliu@hotmail.com>
2018-05-09 22:31:57 -0700

    Added tests

--- src/tests/DatabaseHelperTest.java
+++ src/tests/DatabaseHelperTest.java
@@ -1,42 +1,28 @@
 package tests;
 
-import org.jboss.arquillian.container.test.api.Deployment;
-import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.EmptyAsset;
-import org.jboss.shrinkwrap.api.spec.JavaArchive;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-@RunWith(Arquillian.class)
 public class DatabaseHelperTest {
-    @Deployment
-    public static JavaArchive createDeployment() {
-        return ShrinkWrap.create(JavaArchive.class)
-                .addClass(application.DatabaseHelper.class)
-                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
-    }
 
-    @org.junit.Test
+    @Test
     public void createDatabase() {
-        assertEquals(5,5);
     }
 
-    @org.junit.Test
+    @Test
     public void insertLibrary() {
-        assertEquals("help", "help");
     }
 
-    @org.junit.Test
+    @Test
     public void deleteLibrary() {
     }
 
-    @org.junit.Test
+    @Test
     public void listLibraries() {
     }
 
-    @org.junit.Test
+    @Test
     public void closeDB() {
     }
 }
